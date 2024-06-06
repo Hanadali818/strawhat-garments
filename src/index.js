11214670f9a9c65e57f98db90b5bd1ebae9fcd16
Hanadali818 <104621175+Hanadali818@users.noreply.github.com>
2024-06-06 02:59:26 -0500

    created user context for react context

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 
+import { UserProvider } from './contexts/user.context';
+import App from './App';
+import './index.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
 root.render(
   <React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-    
+    <BrowserRouter>
+      <UserProvider>
+        <App />
+      </UserProvider> 
+    </BrowserRouter>
   </React.StrictMode>
 );
 
