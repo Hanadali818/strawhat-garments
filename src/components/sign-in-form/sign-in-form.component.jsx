d0eba94014f1a1d3fe6e273c267aa0b52d4acb8b
Hanadali818 <104621175+Hanadali818@users.noreply.github.com>
2024-06-14 22:30:03 -0500

    centralized more of the code to know google sign in uses the user context

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,10 +1,9 @@
-import { useState, useContext } from "react";
+import { useState } from "react";
 
 import FormInput from "../form-input/form-input.component";
 
 import Button from "../button/button.component";
 
-import {UserContext} from '../../contexts/user.context'
 
 import { 
     signInWithGooglePopup, 
@@ -18,30 +17,24 @@ const defaultFormFields = {
     email: '',
     password:'',
 }
-
-
 const SignInForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { email, password} = formFields;
-
-    const { setCurrentUser } = useContext(UserContext)
-
 
     const resetFormFields = () => {
         setFormFields(defaultFormFields);
     }
 
     const signInWithGoogle = async () => {
-        const {user} =  await signInWithGooglePopup();
-  
-        await createUserDocumentFromAuth(user);
+       await signInWithGooglePopup();
+      
     }
 
     const handleSubmit = async (event) => {
         event.preventDefault();
 
         try {
-            const {user} = await signInAuthUserWithEmailAndPassword(
+            await signInAuthUserWithEmailAndPassword(
                 email, 
                 password
             );
--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -2,6 +2,7 @@ import { createContext, useState, useEffect } from 'react';
 
 import {
   onAuthStateChangedListener,
+  createUserDocumentFromAuth,
 } from '../utils/firebase/firebase.utils';
 
 export const UserContext = createContext({
@@ -15,7 +16,10 @@ export const UserProvider = ({ children }) => {
 
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
-        console.log(user)
+        if(user) {
+            createUserDocumentFromAuth(user);
+        }
+        setCurrentUser(user);
     });
 
     return unsubscribe;
