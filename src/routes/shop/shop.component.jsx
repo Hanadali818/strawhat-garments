d78e346e035a999137c7982e888096fe282b349c
Hanadali818 <104621175+Hanadali818@users.noreply.github.com>
2024-07-20 20:51:45 -0500

    created the category route folder so i can use paths and parameter to narrow down my search for category page

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
 
 import CategoriesPreview from '../categories-preview/categories-preview.component';
+import Category from '../category/category.comonent';
+
 
 import './shop.styles.scss'
 
@@ -9,6 +11,7 @@ const Shop = () => {
         <div className="shop-container">
             <Routes>
                 <Route index element={<CategoriesPreview/>} />
+                <Route path=':category' element={<Category/>} />
             </Routes>
         </div>
     );
