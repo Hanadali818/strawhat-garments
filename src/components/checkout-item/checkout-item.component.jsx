ea1f9024d25c73a968ca306bdf6bfc0cd3dafa90
Hanadali818 <104621175+Hanadali818@users.noreply.github.com>
2024-07-22 19:52:58 -0500

    Converted checkout-item styles from sass to styled components

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -2,7 +2,16 @@ import { useContext } from 'react';
 
 import { CartContext } from '../../contexts/cart.context';
 
-import './checkout-item.styles.scss'
+import {
+    CheckoutItemContainer,
+    ImageContainer,
+    BaseSpan,
+    Quantity,
+    Arrow,
+    Value,
+    RemoveButton,
+} from './checkout-item.styles'
+
 
 const CheckoutItem = ( {cartItem} ) => {
     const {name, imageUrl, price, quantity} = cartItem;
@@ -14,27 +23,27 @@ const CheckoutItem = ( {cartItem} ) => {
     const removeItemHandler = () => removeItemToCart(cartItem)
 
     return (
-        <div className='checkout-item-container'>
-            <div className='image-container'>
+        <CheckoutItemContainer>
+            <ImageContainer>
                 <img src={imageUrl} alt={`${name}`}/>
-            </div>
-            <span className='name'>{name}</span>
-            <span className='quantity'>
-                <div className='arrow' onClick={removeItemHandler}>
+            </ImageContainer>
+            <BaseSpan>{name}</BaseSpan>
+            <Quantity>
+                <Arrow onClick={removeItemHandler}>
                     &#10094;
-                </div>
-                <span className='value'>
+                </Arrow>
+                <Value>
                     {quantity}
-                </span>
-                <div className='arrow' onClick={addItemHandler}>
+                </Value>
+                <Arrow onClick={addItemHandler}>
                     &#10095;
-                </div>
-            </span>
-            <span className='price'>{price}</span>
-            <div className='remove-button' onClick={clearItemHandler}>
+                </Arrow>
+            </Quantity>
+            <BaseSpan>{price}</BaseSpan>
+            <RemoveButton onClick={clearItemHandler}>
                 &#10005;
-            </div>
-        </div>
+            </RemoveButton>
+        </CheckoutItemContainer>
     )
 }
 
