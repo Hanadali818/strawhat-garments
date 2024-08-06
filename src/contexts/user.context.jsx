b085739e8484e6c3e56bf0b782aa9320358f3b0c
Hanadali818 <104621175+Hanadali818@users.noreply.github.com>
2024-08-05 23:56:16 -0500

    Updated my cart context to use reducer as well as creating an reducer utils file

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,5 +1,7 @@
 import { createContext, useEffect, useReducer } from 'react';
 
+import { createAction } from '../utils/reducer/reducer.utils';
+
 import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
@@ -36,9 +38,8 @@ export const UserProvider = ({ children }) => {
   const [ {currentUser}, dispatch ] = useReducer(userReducer, INITIAL_STATE );
   console.log(currentUser)
   
-  const setCurrentUser = (user) => {
-    dispatch({type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user})
-  }
+  const setCurrentUser = (user) =>
+    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
 
   const value = { currentUser, setCurrentUser };
 
